82a485b76d8a1f5dad7822710d8fab2bf00e83ca
widdowquinn <leightonpritchard@mac.com>
2021-08-30 11:13:23 +0100

    set up RStudio project in this repository

---
+++ install.R
@@ -0,0 +1,13 @@
+install.packages("bookdown")   # for preparation of the HTML
+install.packages("DT")         # for interactive data tables in standalone HTML
+install.packages("GGally")     # for graphical representations that ggplot doesn't do
+install.packages("ggfortify")  # for more things ggplto won't do
+install.packages("ggplot2")    # for pretty data visualisation, and example datasets
+install.packages("ggpubr")     # for ggplot2 multigraph layouts
+install.packages("ggridges")   # for ggplot2 joyplot/ridgeplot/stacked density plots
+install.packages("kableExtra") # for table styling
+install.packages("latex2exp")  # for expressing LaTeX in ggplot 
+install.packages("plotly")     # for interactive graphs in standalone HTML
+install.packages("scales")     # for Kaplan-Meier analysis
+install.packages("survival")   # for Kaplan-Meier analysis
+install.packages("tidyverse")  # for convenient data processing
