df3b122099e8625f3566442f1ba0e099229c5769
widdowquinn <leightonpritchard@mac.com>
2021-10-11 18:46:40 +0100

    add summary stats for R section

--- notebooks/data/summarise_esoph.R
+++ notebooks/data/summarise_esoph.R
@@ -1,3 +1,10 @@
 # Load Oesophagus case/control data
 # First row is a header
-data = read.table("esoph-tab.tsv", header=TRUE, sep="\t")
+data = read.table("esoph-tab.tsv", header=TRUE, sep="\t", stringsAsFactors=TRUE)
+
+# Obtain a summary of each column in the loaded data
+summary(data)
+
+# Calculate standard deviations of the number of cases and controls
+sd(data$ncases)
+sd(data$ncontrols)
