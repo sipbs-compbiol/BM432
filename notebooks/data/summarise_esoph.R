6ce924b88dda450135d2db4b63bb2d5fb46b3ea9
widdowquinn <leightonpritchard@mac.com>
2021-10-12 11:47:32 +0100

    add end of R workshop session

--- notebooks/data/summarise_esoph.R
+++ notebooks/data/summarise_esoph.R
@@ -8,3 +8,21 @@ summary(data)
 # Calculate standard deviations of the number of cases and controls
 sd(data$ncases)
 sd(data$ncontrols)
+
+# Produce a table with counts of ncases and ncontrols for each alcgp category
+aggregate(cbind(ncases_tot=data$ncases, ncontrols_tot=data$ncontrols),
+          by=list(alcgp=data$alcgp), FUN=sum)
+
+# Generate a plot of ncases and ncontrols for each alcgp category
+library(dplyr)
+library(ggplot2)
+library(tidyr)
+dfm = data %>%
+  group_by(alcgp) %>%
+  summarise(ncases=sum(ncases), ncontrol=sum(ncontrols)) %>%
+  pivot_longer(!alcgp, names_to="datatype", values_to="count")
+ggplot(dfm, aes(x=alcgp, y=count)) +
+  geom_bar(aes(fill=datatype), position="dodge", stat="identity")
+
+# Save bar chart as PDF for publication
+ggsave("case_control_counts_by_alcgp.pdf")
